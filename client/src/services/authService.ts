4b7e7eaa5cd84083929b6141a572e796178494b0
Garvit Arora <garvit.university@gmail.com>
2025-09-18 01:47:35 +0530

    Update authService.ts

--- client/src/services/authService.ts
+++ client/src/services/authService.ts
@@ -34,7 +34,7 @@ const authService = {
 
   login: async (email: string, password: string) => {
     try {
-      const response = await axios.post(`${API_BASE_URL}/auth-login`, {
+      const response = await axios.post(`${API_BASE_URL}/auth/login`, {
         email,
         password
       });
